2c37a93b524512944b33162d59a14a0fbd301ee3
Abin Jacob <106917779+abinjakob@users.noreply.github.com>
2025-10-15 13:41:56 +0200

    Add files via upload

--- cueControl.cs
+++ cueControl.cs
@@ -64,6 +64,15 @@ public class cueControl : MonoBehaviour
     private float classifierX;
     private float classifierY;
 
+    // target cue render matkerials for color changing
+    private Renderer Target_1_cue_rend;
+    private Renderer Target_2_cue_rend;
+    private Renderer Target_3_cue_rend;
+    private Renderer Target_4_cue_rend;
+    private Renderer Target_5_cue_rend;
+    private Renderer Target_6_cue_rend;
+    private Color originalColor;
+
 
 
     void Awake()
@@ -98,12 +107,20 @@ public class cueControl : MonoBehaviour
         flickerControl6 = Target_6.GetComponent<flickerControl>();
         flickerPeriod = flickerControl1.trialDuration;
 
+        // get renderer materials
+        Target_1_cue_rend = Target_1_cue.GetComponent<Renderer>();
+        Target_2_cue_rend = Target_2_cue.GetComponent<Renderer>();
+        Target_3_cue_rend = Target_3_cue.GetComponent<Renderer>();
+        Target_4_cue_rend = Target_4_cue.GetComponent<Renderer>();
+        Target_5_cue_rend = Target_5_cue.GetComponent<Renderer>();
+        Target_6_cue_rend = Target_6_cue.GetComponent<Renderer>();
+        originalColor = Target_1_cue_rend.material.color;
+
         // connection manager
         connectionManager = FindObjectOfType<ConnectionManager>();
 
         // create trial sequence
-        //targets = new string[] { "1", "2", "3", "4", "5", "6" };
-        targets = new string[] { "1", "1"};
+        targets = new string[] { "1", "2", "3", "4", "5", "6" };
         trialSequence = new string[trialcount];
         for (int i = 0; i < trialcount; i++)
         {
@@ -161,10 +178,12 @@ public class cueControl : MonoBehaviour
         }
     }
 
+    // function for timeout for listening to the classifier output
     private IEnumerator WaitForClassifierEvent(int expectedCode, float timeoutSeconds)
     {
         float startTime = Time.time;
         classifierEventCode = 0;
+        classifierX = 0;
 
         while (classifierEventCode != expectedCode)
         {
@@ -203,6 +222,7 @@ public class cueControl : MonoBehaviour
             yield return new WaitForSeconds(3);
             // starting trial cue
             Target_1_cue.SetActive(true);
+            yield return new WaitForSeconds(1f);
             if (connectionManager != null)
             {
                 connectionManager.Send("cue_1", "MarkerStream");
@@ -211,10 +231,12 @@ public class cueControl : MonoBehaviour
             else
                 Debug.Log("Not Connected");
 
-
             // wait for classifier to send event 1
             classifierEventCode = 0;
             yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_1_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(1f);
+            Target_1_cue_rend.material.color = originalColor;
             if (connectionManager != null)
             {
                 connectionManager.Send("flk_1", "MarkerStream");
@@ -223,80 +245,248 @@ public class cueControl : MonoBehaviour
             else
                 Debug.Log("Not Connected");
             Target_1_holder.SetActive(false);
-            //Target_1.SetActive(true);
-            //flickerControl1.StartFlickering();
+            Target_1.SetActive(true);
+            flickerControl1.StartFlickering();
             yield return new WaitForSeconds(flickerPeriod);
-
+            Target_1.SetActive(false);
+            Target_1_holder.SetActive(true);
 
             // wait for classifier to send event 2
             classifierEventCode = 0;
             yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
-            //Target_1.SetActive(false);
+            Target_1_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(2f);
             Target_1_cue.SetActive(false);
+            Target_1_cue_rend.material.color = originalColor;
             classifierEventCode = 0;
 
-
-
         }
+
         else if (currentTrial == "2")
         {
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(3f);
+            // starting trial cue
             Target_2_cue.SetActive(true);
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(1f);
+            if (connectionManager != null)
+            {
+                connectionManager.Send("cue_2", "MarkerStream");
+                Debug.Log("Sending marker: " + "cue_2");
+            }
+            else
+                Debug.Log("Not Connected");
+
+
+            // wait for classifier to send event 1
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_2_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(1f);
+            Target_2_cue_rend.material.color = originalColor;
+            if (connectionManager != null)
+            {
+                connectionManager.Send("flk_2", "MarkerStream");
+                Debug.Log("Sending marker: " + "flk_2");
+            }
+            else
+                Debug.Log("Not Connected");
             Target_2_holder.SetActive(false);
             Target_2.SetActive(true);
             flickerControl2.StartFlickering();
             yield return new WaitForSeconds(flickerPeriod);
             Target_2.SetActive(false);
+            Target_2_holder.SetActive(true);
+
+            // wait for classifier to send event 2
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_2_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(2f);
             Target_2_cue.SetActive(false);
+            Target_2_cue_rend.material.color = originalColor;
+            classifierEventCode = 0;
+
         }
+
         else if (currentTrial == "3")
         {
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(3f);
+            // starting trial cue
             Target_3_cue.SetActive(true);
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(1f);
+            if (connectionManager != null)
+            {
+                connectionManager.Send("cue_3", "MarkerStream");
+                Debug.Log("Sending marker: " + "cue_3");
+            }
+            else
+                Debug.Log("Not Connected");
+
+
+            // wait for classifier to send event 1
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_3_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(1f);
+            Target_3_cue_rend.material.color = originalColor;
+            if (connectionManager != null)
+            {
+                connectionManager.Send("flk_3", "MarkerStream");
+                Debug.Log("Sending marker: " + "flk_3");
+            }
+            else
+                Debug.Log("Not Connected");
             Target_3_holder.SetActive(false);
             Target_3.SetActive(true);
             flickerControl3.StartFlickering();
             yield return new WaitForSeconds(flickerPeriod);
             Target_3.SetActive(false);
+            Target_3_holder.SetActive(true);
+
+            // wait for classifier to send event 2
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_3_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(2f);
             Target_3_cue.SetActive(false);
+            Target_3_cue_rend.material.color = originalColor;
+            classifierEventCode = 0;
         }
+
         else if (currentTrial == "4")
         {
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(3f);
+            // starting trial cue
             Target_4_cue.SetActive(true);
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(1f);
+            if (connectionManager != null)
+            {
+                connectionManager.Send("cue_4", "MarkerStream");
+                Debug.Log("Sending marker: " + "cue_4");
+            }
+            else
+                Debug.Log("Not Connected");
+
+
+            // wait for classifier to send event 1
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_4_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(1f);
+            Target_4_cue_rend.material.color = originalColor;
+            if (connectionManager != null)
+            {
+                connectionManager.Send("flk_4", "MarkerStream");
+                Debug.Log("Sending marker: " + "flk_4");
+            }
+            else
+                Debug.Log("Not Connected");
             Target_4_holder.SetActive(false);
             Target_4.SetActive(true);
             flickerControl4.StartFlickering();
             yield return new WaitForSeconds(flickerPeriod);
             Target_4.SetActive(false);
+            Target_4_holder.SetActive(true);
+
+            // wait for classifier to send event 2
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_4_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(2f);
             Target_4_cue.SetActive(false);
+            Target_4_cue_rend.material.color = originalColor;
+            classifierEventCode = 0;
         }
+
         else if (currentTrial == "5")
         {
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(3f);
+            // starting trial cue
             Target_5_cue.SetActive(true);
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(1f);
+            if (connectionManager != null)
+            {
+                connectionManager.Send("cue_5", "MarkerStream");
+                Debug.Log("Sending marker: " + "cue_5");
+            }
+            else
+                Debug.Log("Not Connected");
+
+
+            // wait for classifier to send event 1
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_5_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(1f);
+            Target_5_cue_rend.material.color = originalColor;
+            if (connectionManager != null)
+            {
+                connectionManager.Send("flk_5", "MarkerStream");
+                Debug.Log("Sending marker: " + "flk_5");
+            }
+            else
+                Debug.Log("Not Connected");
             Target_5_holder.SetActive(false);
             Target_5.SetActive(true);
             flickerControl5.StartFlickering();
             yield return new WaitForSeconds(flickerPeriod);
             Target_5.SetActive(false);
+            Target_5_holder.SetActive(true);
+
+            // wait for classifier to send event 2
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_5_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(2f);
             Target_5_cue.SetActive(false);
+            Target_5_cue_rend.material.color = originalColor;
+            classifierEventCode = 0;
         }
+
         else if (currentTrial == "6")
         {
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(3f);
+            // starting trial cue
             Target_6_cue.SetActive(true);
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(1f);
+            if (connectionManager != null)
+            {
+                connectionManager.Send("cue_6", "MarkerStream");
+                Debug.Log("Sending marker: " + "cue_6");
+            }
+            else
+                Debug.Log("Not Connected");
+
+
+            // wait for classifier to send event 1
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_6_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(1f);
+            Target_6_cue_rend.material.color = originalColor;
+            if (connectionManager != null)
+            {
+                connectionManager.Send("flk_6", "MarkerStream");
+                Debug.Log("Sending marker: " + "flk_6");
+            }
+            else
+                Debug.Log("Not Connected");
             Target_6_holder.SetActive(false);
             Target_6.SetActive(true);
             flickerControl6.StartFlickering();
             yield return new WaitForSeconds(flickerPeriod);
             Target_6.SetActive(false);
+            Target_6_holder.SetActive(true);
+
+
+            // wait for classifier to send event 2
+            classifierEventCode = 0;
+            yield return StartCoroutine(WaitForClassifierEvent(1, 5f));
+            Target_6_cue_rend.material.color = (classifierX != 0) ? Color.red : Color.green;
+            yield return new WaitForSeconds(2f);
             Target_6_cue.SetActive(false);
+            Target_6_cue_rend.material.color = originalColor;
+            classifierEventCode = 0;
         }
 
         Target_1_holder.SetActive(false);
